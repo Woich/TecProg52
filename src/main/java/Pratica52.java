8c5b740aa7fecc7db89a4777602dff60c165f9b1
Woich <minewoichbr@gmail.com>
2017-10-20 00:04:59 -0200

    Tentativa 1

--- src/main/java/Pratica52.java
+++ src/main/java/Pratica52.java
@@ -1,3 +1,6 @@
+
+import utfpr.ct.dainf.if62c.pratica.Equacao2Grau;
+
 /**
  * UTFPR - Universidade Tecnológica Federal do Paraná
  * DAINF - Departamento Acadêmico de Informática
@@ -7,6 +10,37 @@
  */
 public class Pratica52 {
     public static void main(String[] args) {
-        System.out.println("Olá, Java!");
+        Equacao2Grau<Integer> equacao;
+        
+        //raizes diferentes
+        try{
+            equacao = new Equacao2Grau<>(1,-3,1);
+            System.out.println("A equação é x^2 - 3x + 1");
+            System.out.println("As raízes são "+equacao.getRaiz1()+" e "+equacao.getRaiz2());
+        }
+        catch(Throwable t){
+            System.out.println(t.getMessage());
+        }
+        
+        //raizes iguais
+        try{
+            equacao = new Equacao2Grau<>(2,4,2);
+            System.out.println("A equação é 2x^2 + 4x + 2");
+            System.out.println("As raízes são "+equacao.getRaiz1()+" e "+equacao.getRaiz2());
+        }
+        catch(Throwable t){
+            System.out.println(t.getMessage());
+        }
+        
+        //sem raízes
+        try{
+            equacao = new Equacao2Grau<>(1,2,3);
+            System.out.println("A equação é x^2 + 2x + 3");
+            System.out.println("As raízes são "+equacao.getRaiz1()+" e "+equacao.getRaiz2());
+        }
+        catch(Throwable t){
+            System.out.println(t.getMessage());
+        }
     }
 }
+
--- src/main/java/utfpr/ct/dainf/if62c/pratica/Equacao2Grau.java
+++ src/main/java/utfpr/ct/dainf/if62c/pratica/Equacao2Grau.java
@@ -9,6 +9,84 @@ package utfpr.ct.dainf.if62c.pratica;
  *
  * @author woich
  */
-public class Equacao2Grau {
+public class Equacao2Grau <T extends Number>{
+
+    private T a;
+    private T b;
+    private T c;
     
+    public Equacao2Grau(T a, T b, T c) throws RuntimeException{
+        if(((Number)a).doubleValue() == 0.0){
+            throw new RuntimeException("Valor de 'a' não pode ser 0");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    
+    /**
+     * @return the a
+     */
+    public T getA() {
+        return a;
+    }
+
+    /**
+     * @param a the a to set
+     */
+    public void setA(T a) throws RuntimeException{
+        if(((Number)a).doubleValue() == 0.0){
+            throw new RuntimeException("Valor de 'a' não pode ser 0");
+        }
+        
+        this.a = a;
+    }
+
+    /**
+     * @return the b
+     */
+    public T getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(T b) {
+        this.b = b;
+    }
+
+    /**
+     * @return the c
+     */
+    public T getC() {
+        return c;
+    }
+
+    /**
+     * @param c the c to set
+     */
+    public void setC(T c) {
+        this.c = c;
+    }
+    
+    public double getRaiz1() throws RuntimeException{
+        
+        if((Math.pow(b.doubleValue(), 2)-4*a.doubleValue()*c.doubleValue())/2 < 0){
+            throw new RuntimeException("Equação não tem solução real");
+        }
+        
+        return (-b.doubleValue() + Math.sqrt( (Math.pow(b.doubleValue(), 2)-4*a.doubleValue()*c.doubleValue() ) ))/2;
+        
+    }
+    /*---------------------------------------------------------------------------------------------------------------*/
+    public double getRaiz2() throws RuntimeException{
+        
+        if((Math.pow(b.doubleValue(), 2)-4*a.doubleValue()*c.doubleValue())/2 < 0){
+            throw new RuntimeException("Equação não tem solução real");
+        }
+        
+        return (-b.doubleValue() - Math.sqrt( (Math.pow(b.doubleValue(), 2)-4*a.doubleValue()*c.doubleValue() ) ))/2;
+        
+    }
 }
